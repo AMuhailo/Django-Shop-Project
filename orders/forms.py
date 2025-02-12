3c98cb5f0f97ba0300387e468d3fd7b55802e530
amuhailo25@gmail.com <154220955+AMuhailo@users.noreply.github.com>
2025-02-12 21:20:14 +0200

    🟩Create order form

---
+++ orders/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+from django.forms import modelform_factory
+from .models import Order
+
+OrderForm = modelform_factory(Order, exclude=['created','updated','paid'])
--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from itertools import product
+from django.shortcuts import redirect, render
+from django.views.decorators.http import require_POST
+from cart.cart import Cart
+from .models import Order, Items
+from .forms import OrderForm
 
 # Create your views here.
+def create_order(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                Items.objects.create(product = item['product'],
+                                     order = order,
+                                     quantity = item['quantity'],
+                                     price = item['price'])
+            cart.clear()
+            return redirect('shop:product_list_url')
+    else:
+        form = OrderForm()
+    context = {
+        'cart':cart,
+        'form':form
+    }
+    return render(request,'pages/orders/orders.html', context)
+        
