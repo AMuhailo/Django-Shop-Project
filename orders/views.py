1cd7c72dde51b3cdc3a5b3d1022db7b32026f24b
amuhailo25@gmail.com <154220955+AMuhailo@users.noreply.github.com>
2025-02-14 01:23:35 +0200

    ❌Don`t used Celery in projects

--- orders/views.py
+++ orders/views.py
@@ -3,7 +3,6 @@ from django.views.decorators.http import require_POST
 from cart.cart import Cart
 from .models import Order, Items
 from .forms import OrderForm
-from .tasks import send_order
 
 # Create your views here.
 def create_order(request):
@@ -18,7 +17,6 @@ def create_order(request):
                                      quantity = item['quantity'],
                                      price = item['price'])
             cart.clear()
-            send_order.delay(order.id)
             return redirect('shop:product_list_url')
     else:
         form = OrderForm()
