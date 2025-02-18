173869e4870158c67e04b8fd202c9970f479d39d
amuhailo25@gmail.com <154220955+AMuhailo@users.noreply.github.com>
2025-02-18 14:23:23 +0200

    🟩Celery setting and works

--- orders/tasks.py
+++ orders/tasks.py
@@ -1,12 +1,16 @@
 from django.shortcuts import get_object_or_404
 from celery import shared_task
-from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.core.mail import EmailMultiAlternatives
 
 from orders.models import Order
 @shared_task
 def order_sender(order_id):
     order = get_object_or_404(Order, id = order_id)
-    sender = f"You orders number {order.id}"
-    message = f"Orders done {order.id}"
-    mail = send_mail(sender, message,'admin@gmail.com', [order.email])
-    return mail
+    sender = f"You orders №{order.id}"
+    html_message = render_to_string('pages/orders/emailorder.html', {"order":order})
+    message = f"Thank you for your order #{order.id}. Total: ${order.get_total_money}"
+    mail = EmailMultiAlternatives(sender, message,'admin@gmail.com', [order.email])
+    mail.attach_alternative(html_message,'text/html')
+    mail.send()
+    return f"Email sent to {order.email}"
