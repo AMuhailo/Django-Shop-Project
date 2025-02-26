4bebcc407f677785200fbe8b2ad6fee7054955b0
amuhailo25@gmail.com <154220955+AMuhailo@users.noreply.github.com>
2025-02-26 18:43:48 +0200

    🟨Updated forms

--- shop/forms.py
+++ shop/forms.py
@@ -3,8 +3,32 @@ from django.forms import modelform_factory
 from .models import Category, Product
 
 ProductCreateFormset = modelform_factory(model = Product, 
-                                   exclude = ['slug','active','created','updated','discount'])
+                                   exclude = ['slug','active','created','updated','discount'],
+                                   widgets={
+                                       'category':forms.Select(attrs={
+                                           'class':'form-select',
+                                           'id':'floatingSelect',
+                                       }),
+                                       'image':forms.FileInput(attrs={
+                                           'class':'form-control',
+                                           'id':'inputGroupFile01'
+                                       }),
+                                        'title':forms.TextInput(attrs={
+                                            "class":"form-control",
+                                            'placeholder':'Title product...',
+                                        }),
+                                        'description':forms.Textarea(attrs={
+                                            'class':'form-control',
+                                            'placeholder':'Description product'
+                                        }),
+                                        'price':forms.NumberInput(attrs={
+                                            'class':'form-control',
+                                            'value':'0'
+                                        }) 
+                                   })
 
 
 class SearchForm(forms.Form):
-    query = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control",'placeholder':'search...'}))
+    query = forms.CharField(required=False, widget=forms.TextInput(attrs={"class":"form-control",
+                                                                          'placeholder':'search...',
+                                                                          "aria-describedby":"button-addon1"}))
